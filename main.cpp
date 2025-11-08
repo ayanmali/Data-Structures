aad1d7a2462e331660f39a6da33525fcb524f5c7
Ayan Ali <ayan_ali2004@hotmail.com>
2025-11-08 15:44:00 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template <typename T>
+class SwissTable {
+    private:
+    public:
+
+};
+
+int main() {
+    std::cout << "Swiss Table in C++" << std::endl;
+    return 0;
+};
