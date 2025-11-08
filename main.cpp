8781e03c12465f31b57e2dc7a65e131a28721deb
Ayan Ali <ayan_ali2004@hotmail.com>
2025-11-08 16:36:02 -0500

    added function declarations + cmake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <functional>
+#include <vector>
 
-template <typename T>
-class SwissTable {
+/*
+Hash table based on hybrid chaining + open addressing
+Each key is associated with a bucket index.
+Each key's value is a pointer to its data
+*/
+template <typename K, typename V>
+class HashTable {
     private:
-    public:
+        size_t size;
+        std::function<size_t(K)> hashFunction;
+        std::vector<std::pair<K, V>> table;
 
+        //K computeHash(K key) {};
+    public:
+        HashTable(size_t size) {
+            this->size = size;
+            this->table.resize(size);
+        }
+        V get(K key) {};
+        void set(K key, V value) {};
+        void remove(K key) {};
+        bool contains(K key) {};
 };
 
 int main() {
-    std::cout << "Swiss Table in C++" << std::endl;
+    std::cout << "Hash Table based on hybrid chaining + open addressing in C++" << std::endl;
     return 0;
 };
