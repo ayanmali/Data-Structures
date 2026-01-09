81ab08381653e61ea3712d564a02b7cefecc5245
Ayan Ali <ayan_ali2004@hotmail.com>
2026-01-08 22:29:04 -0500

    added custom smart pointer deleter functions

--- arena-allocator/arena_alloc.cpp
+++ arena-allocator/arena_alloc.cpp
@@ -1,6 +1,10 @@
 #include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <memory>
 #include <vector>
 
+// TODO: add custom deleters to return memory back to the arena after destruction
 struct ArenaAllocator {
     std::vector<char> data;
     std::size_t current_offset;
@@ -20,14 +24,48 @@ struct ArenaAllocator {
 };
 
 template <typename T, typename... Args>
-std::unique_ptr<T> make_unique_arena(ArenaAllocator& allocator, Args&&... args) {
-    T* ptr = static_cast<T*>(allocator.allocate(sizeof(T), alignof(T)));
-    return std::unique_ptr<T>(ptr);
+auto make_unique_arena(ArenaAllocator& allocator, Args&&... args) {
+    void* ptr = allocator.allocate(sizeof(T), alignof(T));
+    const auto deleter = [&allocator](T* p) {
+        // call destructor
+        p->~T();
+        // clear the memory and update the allocator's offset
+        std::memset(p, 0, sizeof(T));
+        allocator.current_offset -= sizeof(T);
+    };
+
+    // construct the object in the allocated memory (placement new)
+    new (ptr) T(std::forward<Args>(args)...);
+
+    return std::unique_ptr<T, decltype(deleter)>(static_cast<T*>(ptr), deleter);
 };
 
 
 template <typename T, typename... Args>
-std::unique_ptr<T> make_shared_arena(ArenaAllocator& allocator, Args&&... args) {
-    T* ptr = static_cast<T*>(allocator.allocate(sizeof(T), alignof(T)));
-    return std::shared_ptr<T>(ptr);
+std::shared_ptr<T> make_shared_arena(ArenaAllocator& allocator, Args&&... args) {
+    void* ptr = allocator.allocate(sizeof(T), alignof(T));
+    const auto deleter = [&allocator](T* p) {
+        // call destructor
+        p->~T();
+        // clear the memory and update the allocator's offset
+        std::memset(p, 0, sizeof(T));
+        allocator.current_offset -= sizeof(T);
+    };
+
+    // construct the object in the allocated memory (placement new)
+    new (ptr) T(std::forward<Args>(args)...);
+
+    auto shared_ptr = std::shared_ptr<T>(static_cast<T*>(ptr), deleter);
+
+    return shared_ptr;
 };
+
+int main() {
+    ArenaAllocator allocator(sizeof(int) * 10);
+    std::cout << "Arena Allocator\n";
+    auto p = make_unique_arena<int>(allocator, 42);
+    std::cout << "p: " << *p << "\n";
+    auto q = make_shared_arena<int>(allocator, 42);
+    std::cout << "q: " << *q << "\n";
+
+}
